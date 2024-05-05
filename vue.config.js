27942aa215f1b9a3e6643c5a77835167d1c9cc70
Chorok Oh <157563710+chorok5@users.noreply.github.com>
2024-05-05 21:47:49 +0900

    Delete vue.config.js

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  outputDir: '../src/main/resources/static', // Build Directory
-	devServer: {
-		proxy: 'http://localhost' // Spring Boot Server
-	}
-
-})
