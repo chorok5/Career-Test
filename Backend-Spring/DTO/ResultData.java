64fc4513e84bc822d97e87e6196a6ae23945f1b8
Chorok Oh <157563710+chorok5@users.noreply.github.com>
2024-05-17 14:34:18 +0900

    심리검사 DTO
    응답을 배열로 받아올 수 있도록 private List<Integer> userAnswersArray; 추가

--- Backend-Spring/DTO/ResultData.java
+++ Backend-Spring/DTO/ResultData.java
@@ -3,10 +3,15 @@ package com.kidwiz.web.DTO;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 // ResultData(DTO) 클래스는 TestQuestion, TestAnswer, TestResult 엔티티 간의 데이터 전송을 위해 사용
 
+@NoArgsConstructor
+@JsonAutoDetect
 @Data
 
 public class ResultData {
@@ -14,4 +19,6 @@ public class ResultData {
     private String recommendedJobs;
     private List<String> personalTraits;
     private LocalDateTime tdate;
+    
+    private List<Integer> userAnswersArray;
 }
