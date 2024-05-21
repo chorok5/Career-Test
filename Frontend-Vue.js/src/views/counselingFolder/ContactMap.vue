91234e34343765019db757dc4734bfcef0f450b8
Chorok Oh <157563710+chorok5@users.noreply.github.com>
2024-05-21 21:07:54 +0900

    심리검사 하위 메뉴
    안내,신청 등

---
+++ Frontend-Vue.js/src/views/counselingFolder/ContactMap.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <MenuPage />
+    <SimriSecMenu />
+    <h1>Contact 게시판 입니다. 위치 지도 불러오기</h1>
+  </div>
+</template>
+
+<script>
+import MenuPage from '@/components/MenuPage.vue';
+import SimriSecMenu from '@/views/counselingFolder/SimriSecMenu.vue';
+
+export default {
+  name: 'ContactMap',
+    components: {
+    MenuPage,
+    SimriSecMenu
+  }
+  
+};
+</script>
+<style scoped>
+
+</style>
