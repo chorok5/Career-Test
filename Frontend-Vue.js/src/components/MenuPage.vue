6e3b43ab8fef2056a34a1ecfaa5c2f1c2bc0f027
Chorok Oh <157563710+chorok5@users.noreply.github.com>
2024-05-24 20:53:15 +0900

    Add files via upload

--- Frontend-Vue.js/src/components/MenuPage.vue
+++ Frontend-Vue.js/src/components/MenuPage.vue
@@ -1,7 +1,6 @@
 <template>
 <div>
   <header>
-
     <div class="nav">
       <nav class="nav-top">
         <span class="top-left">
@@ -9,42 +8,66 @@
           <a href="">facebook</a>
         </span>
         <span class="mainImg">
-          <h1>
+          <h1><br>
             <router-link to="/">
-              대충 대표 이미지
+              <img src="@/assets/kidwiz1.png" alt="" style="width:257px; height:80px;">
             </router-link>
-          </h1>
+          </h1><br>
         </span>
         <span class="top-right">
-          <a href="">로그인</a>
-          <a href="">회원가입</a>
-        </span>
-      </nav>
-      <nav class="nav-bot">
-        <ul>
-          <li><router-link to="/testVue">테스트뷰</router-link></li>
-          <li><router-link to="/listTest">DB테스트</router-link></li>
-          <li><router-link to="/BoardList">박선우</router-link></li>
-          <li><router-link to="/insertTest">insert 테스트</router-link></li>
-          <li><router-link to="/regRev">박시호</router-link></li>
-          <li><router-link to="/rsrvTest4">예약테스트</router-link></li>
-          <li><router-link to="/GroupList">박선우 집단</router-link></li>
-          <li><router-link to="/jobConsulting">취업상담</router-link></li>
-          <li v-if="$store.state.account.id"><router-link to="/mypage">마이페이지</router-link></li>
-          <li>
           <router-link to="/login" v-if="!$store.state.account.id">로그인</router-link>
           <a to="/login" @click="logout()" v-else>로그아웃</a>
-          </li>
-          <li><router-link to="/">Home</router-link></li>
-          <li><router-link to="/counseling">심리상담 안내</router-link></li>
-          <li><router-link to="/counselorshow">상담사 소개</router-link></li>
-          <li><router-link to="/test">Test</router-link></li>
+          <router-link to="/mypage" v-if="$store.state.account.id">| 마이페이지</router-link>          
+          <a href="">| English</a>
+        </span>
+      </nav>
+      <nav class="nav-bot boundary">
+        <ul @mouseover="navAct()" @mouseleave="navDead()">
+          <li>센터소개</li>
+          <li>상담사소개</li>
+          <li>상담실</li>
+          <li>상담백과</li>
+          <li>공지사항</li>
+        </ul>
+      </nav>
+    <div class="secNav boundary" :style="{display: navDisplay}" @mouseover="navAct()" @mouseleave="navDead()">
+      <nav class="nav-sec">
+        <ul name="introduce">        
+          <li><router-link to="/admin">관리자페이지</router-link></li>
+          <li> </li>
+          <li> </li>
+        </ul>
+        <ul name="counselors">
+          <li><router-link to="/counselorList">상담사 소개</router-link></li>
+          <li><router-link to="/rsrvTest4">예약테스트</router-link></li>          
+          <li><router-link to="/secMenuTest">다증레이아웃</router-link></li>
+          <li></li>
+          <li></li>
+        </ul>
+        <ul name="counsel">
+          <li><router-link to="/GroupList">집단상담</router-link></li>
+          <li><router-link to="/jobs/intro">취업상담</router-link></li>
+          <li><router-link to="/counseling">심리상담</router-link></li>          
+          <li><router-link to="/regIntroduce">전문상담</router-link></li>           
+          <li><router-link to="/boardlist">게시판상담</router-link></li>
+          <li></li>
+        </ul>
+        <ul name="alpha">
+          <li><router-link to="/cardNews">카드뉴스</router-link></li>
+          <li><router-link to="/applyForm3">applyForm3</router-link></li>
+          <li><router-link to="/resultWrite">resultWrite</router-link></li>
+          <li><router-link to="/regResult">regResult</router-link></li>
+          <li></li>
+        </ul>
+        <ul name="notice">
           <li><router-link to="/faq">FAQ</router-link></li>
           <li><router-link to="/contact">Contact</router-link></li>
-          <li><router-link to="/admin">관리자페이지</router-link></li>
+          <li></li>
+          <li></li>
         </ul>
       </nav>
     </div>
+  </div>
   </header>
   <div class="mainContainer">
     <main>
@@ -56,13 +79,31 @@
 
 
 <script>
-import store from "@/views/store";
+import store from "@/store.js";
 import router from '@/router'
 import axios from 'axios';
+import { onMounted, ref } from 'vue';
 
 export default {
     name:'MenuPage',
+
+    data(){
+      return{
+        navDisplay : 'none'
+      };
+    },
+    methods : {
+      navAct(){
+        this.navDisplay = 'block';
+      },
+      navDead(){
+        this.navDisplay = "none";
+      }
+    },
     setup() {
+
+      const check = ref()
+
       const logout = () => {
         axios.post("http://localhost:3000/api/account/logout", {}, { withCredentials: true })
         .then(()=>{
@@ -71,7 +112,17 @@ export default {
         router.push({path: '/'});
         });
       }
-      return {logout}
+
+      onMounted(()=>{
+        axios.post("/api/admin/admincheck",{withCredentials:true}).then((res)=>{
+          if(res.data==1){
+            check.value = true
+          }else{
+            check.value = false
+          }
+        })
+      })
+      return {logout,check}
     }
 }
 </script>
@@ -80,11 +131,15 @@ export default {
 .nav{
   top: 0;
   width: 100%;
-  background-color: skyblue;
+  background-color: #fff;
+  border-bottom: 1px solid #c0c0c0;
+  position: fixed;
+  z-index: 999;
 }
 .nav-top{
   height: 80px;
-  width: 100%;
+  width: 70%;
+  margin: 0 auto; /* 가운데 정렬 */
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -92,14 +147,35 @@ export default {
 .nav-bot{
   height: 55px;
   width: 100%;
-  background-color: grey;
+  /* background-color: #fff; */
   float: hide;
 }
+.nav-bot ul{
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  display: flex;
+}
 .nav-bot ul li{
+  width: 20%;
   list-style-type: none;
   text-decoration: none;
+  justify-content: center;
+  align-content: center;
+}
+.secNav{
+  width: 100%;
+}
+.nav-sec ul{
+  width: 20%;
+  padding: 0;
   float: left;
-  margin: 0 5px;
+  border-left: 1px solid #d6d6d6;
+}
+.nav-sec ul li{
+  list-style-type: none;
+  text-decoration: none;
+  margin-top: 10px;  
 }
 .top-left{
   margin-right: auto;
@@ -132,4 +208,8 @@ a:hover {
 a:active {
   color : black;
 }
-</style>
+.mainContainer{
+  margin-top: 205px;
+}
+
+</style>@/store@/store.js
