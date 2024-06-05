3a5ff9567cb8b0c2118a0608878fe2204a76bb9c
Chorok Oh <157563710+chorok5@users.noreply.github.com>
2024-06-05 14:43:35 +0900

    Update TestQuestion.java

--- Backend-Spring/DTO/TestQuestion.java
+++ Backend-Spring/DTO/TestQuestion.java
@@ -15,21 +15,21 @@ import lombok.Data;
 @Table(name = "test_question")
 @Comment("심리검사 카테고리와 질문(tid=테스트id, qid=질문번호)")
 public class TestQuestion {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-    @Column(name="qid", nullable=true)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "qid", nullable = true)
     @Comment("질문 id")
     private Integer qid;
-    
-	@Column(name = "tcategory")
-	@Comment("카테고리")
-	private String tcategory;
 
-	@Column(name = "ttitle")
-	@Comment("질문 내용")
-	private String ttitle;
+    @Column(name = "tcategory")
+    @Comment("카테고리")
+    private String tcategory;
+
+    @Column(name = "ttitle")
+    @Comment("질문 내용")
+    private String ttitle;
 
 }
