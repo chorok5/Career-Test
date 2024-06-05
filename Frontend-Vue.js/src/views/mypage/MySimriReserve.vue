44c37b7cb3d52cd04652709659580017eedd4725
Chorok Oh <157563710+chorok5@users.noreply.github.com>
2024-06-05 14:58:08 +0900

    Update MySimriReserve.vue

--- Frontend-Vue.js/src/views/mypage/MySimriReserve.vue
+++ Frontend-Vue.js/src/views/mypage/MySimriReserve.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-        <MySidebar></MySidebar>
-    <MenuPage/>
+    <MySidebar></MySidebar>
+    <MenuPage />
     <div class="mainContainer">
       <div class="row">
         <main class="">
@@ -77,19 +77,23 @@ export default {
 .table {
   margin-top: 20px;
 }
-.line{
-  width: 40%;  
+
+.line {
+  width: 40%;
 }
-.title{
+
+.title {
   text-align: left;
   font-family: 'sj';
 }
-.mainContainer{
+
+.mainContainer {
   width: 100%;
   max-width: 1200px;
   margin: 0 auto;
 }
-.tr{
+
+.tr {
   font-family: 'sjl';
 }
 </style>
