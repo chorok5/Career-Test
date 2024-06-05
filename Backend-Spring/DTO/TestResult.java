acd2614f03a58cdcbd78422e622b339db70bd150
Chorok Oh <157563710+chorok5@users.noreply.github.com>
2024-06-05 14:43:57 +0900

    Update TestResult.java

--- Backend-Spring/DTO/TestResult.java
+++ Backend-Spring/DTO/TestResult.java
@@ -21,29 +21,29 @@ import lombok.Data;
 @Table(name = "test_results")
 @Comment("심리검사 결과 테이블(tno=테스트no, sid=학번)")
 public class TestResult {
-	
+
     @Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Comment("테스트 번호")
     private Integer tno;
-    
+
     @Column(name = "sid", nullable = false)
     @Comment("학번")
     private Integer sid = 1;
-	
-    @Column(name="qid", nullable=false)
+
+    @Column(name = "qid", nullable = false)
     @Comment("질문 id")
     private Integer qid;
-    
-	@Column(name = "tanswer", nullable = false) // vue 에서 value 를 받아와야함.
-	@Comment("응답 value")
-	private Integer tanswer;
-    
+
+    @Column(name = "tanswer", nullable = false) // vue 에서 value 를 받아와야함.
+    @Comment("응답 value")
+    private Integer tanswer;
+
     @Column(name = "total_score", nullable = false)
     @Comment("총점")
     private int totalScore;
 
-    
+
     @Column(name = "member_id")
     @Comment("회원 ID")
     private int memberId;
@@ -52,59 +52,59 @@ public class TestResult {
     @Comment("검사 실시일")
     private LocalDateTime tdate;
 
-	public Integer getTno() {
-		return tno;
-	}
+    public Integer getTno() {
+        return tno;
+    }
 
-	public void setTno(Integer tno) {
-		this.tno = tno;
-	}
+    public void setTno(Integer tno) {
+        this.tno = tno;
+    }
 
-	public Integer getSid() {
-		return sid;
-	}
+    public Integer getSid() {
+        return sid;
+    }
 
-	public void setSid(Integer sid) {
-		this.sid = sid;
-	}
+    public void setSid(Integer sid) {
+        this.sid = sid;
+    }
 
-	public Integer getQid() {
-		return qid;
-	}
+    public Integer getQid() {
+        return qid;
+    }
 
-	public void setQid(Integer qid) {
-		this.qid = qid;
-	}
+    public void setQid(Integer qid) {
+        this.qid = qid;
+    }
 
-	public Integer getTanswer() {
-		return tanswer;
-	}
+    public Integer getTanswer() {
+        return tanswer;
+    }
 
-	public void setTanswer(Integer tanswer) {
-		this.tanswer = tanswer;
-	}
+    public void setTanswer(Integer tanswer) {
+        this.tanswer = tanswer;
+    }
 
-	public int getTotalScore() {
-		return totalScore;
-	}
+    public int getTotalScore() {
+        return totalScore;
+    }
 
-	public void setTotalScore(int totalScore) {
-		this.totalScore = totalScore;
-	}
+    public void setTotalScore(int totalScore) {
+        this.totalScore = totalScore;
+    }
 
-	public int getMemberId() {
-		return memberId;
-	}
+    public int getMemberId() {
+        return memberId;
+    }
 
-	public void setMemberId(int memberId) {
-		this.memberId = memberId;
-	}
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
 
-	public LocalDateTime getTdate() {
-		return tdate;
-	}
+    public LocalDateTime getTdate() {
+        return tdate;
+    }
 
-	public void setTdate(LocalDateTime tdate) {
-		this.tdate = tdate;
-	}
+    public void setTdate(LocalDateTime tdate) {
+        this.tdate = tdate;
+    }
 }
