219eac45609da4fadc8424a0470fe177d21e95ec
Chorok Oh <157563710+chorok5@users.noreply.github.com>
2024-06-05 14:48:20 +0900

    Update JoblistsService.java

--- Backend-Spring/service/JoblistsService.java
+++ Backend-Spring/service/JoblistsService.java
@@ -26,18 +26,11 @@ public class JoblistsService {
         Elements jobElements = document.select(".c_row"); // 공고 리스트 선택
 
         for (Element jobElement : jobElements) {
-            String title = jobElement.select(".cell_mid .cl_top a").text();  // 채용 제목
-            String company = jobElement.select(".cell_first .cpname").text();  // 회사 이름
-            String location = jobElement.select(".cell_mid .cl_md span:nth-of-type(3)").text();  // 위치
-            String date = jobElement.select(".cell_last .cl_btm span:nth-of-type(1)").text();  // 마감 날짜
-            String detailLink = jobElement.select(".cell_mid .cl_top a").attr("href");  // 자세히 보기 링크
-
-            // Debug logging
-//            System.out.println("Title: " + title);
-//            System.out.println("Company: " + company);
-//            System.out.println("Location: " + location);
-//            System.out.println("Date: " + date);
-//            System.out.println("Detail Link: " + detailLink);
+            String title = jobElement.select(".cell_mid .cl_top a").text(); 
+            String company = jobElement.select(".cell_first .cpname").text();  
+            String location = jobElement.select(".cell_mid .cl_md span:nth-of-type(3)").text();  
+            String date = jobElement.select(".cell_last .cl_btm span:nth-of-type(1)").text();  
+            String detailLink = jobElement.select(".cell_mid .cl_top a").attr("href"); 
 
             JoblistsDTO job = new JoblistsDTO(title, company, location, date, detailLink);
             jobs.add(job);
@@ -57,7 +50,6 @@ public class JoblistsService {
                 .filter(this::isValidJob)
                 .collect(Collectors.toList());
 
-        
         // 접수 기간 기준으로 정렬
         filteredJobs.sort(Comparator.comparing(JoblistsDTO::getDate));
         return filteredJobs;
